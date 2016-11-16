0b9fc802003a4321d4fb30d0ef92a31a558b8036
jam42 <radiosilence42@gmail.com>
2016-11-16 20:05:00 +0600

    adding functionality as timetable

--- bot.py
+++ bot.py
@@ -4,9 +4,16 @@ import telebot
 
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    	bot.reply_to(message, "Hi!")
 
-if __name__ == '__main__':
-     bot.polling(none_stop=True)
+@bot.message_handler(commands=['понедельник'])
+def send_message(message):
+	bot.reply_to(message, "1-я неделя: \n 1-я пара: Шабалин (2к, 5 этаж) \n 2-я пара: Шабалин (2к, 5 этаж)\n \n 2-я неделя: \n 1-я пара: Шаталин (2к, 2 этаж)\n 2-я пара: Шаталин (2к, 2 этаж)")
+
+@bot.message_handler(content_types=['text'])
+def echo_all(message):
+    bot.send_message(message.chat.id, ')')
+
+bot.polling()
