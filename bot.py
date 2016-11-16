8217a77a184c0698010b5678206037085187df81
jam42 <radiosilence42@gmail.com>
2016-11-16 20:08:22 +0600

    adding functionality as timetable (fix)

--- bot.py
+++ bot.py
@@ -6,11 +6,11 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    	bot.reply_to(message, "Hi!")
+    	bot.reply_to(message, "Че")
 
 @bot.message_handler(commands=['понедельник'])
 def send_message(message):
-	bot.reply_to(message, "1-я неделя: \n 1-я пара: Шабалин (2к, 5 этаж) \n 2-я пара: Шабалин (2к, 5 этаж)\n \n 2-я неделя: \n 1-я пара: Шаталин (2к, 2 этаж)\n 2-я пара: Шаталин (2к, 2 этаж)")
+	bot.reply_to(message, "1-я неделя: \n 1-я пара: Шаталин (лекция) (2к, 2 этаж) \n 2-я пара: Шаталин (семинар) (2к, 2 этаж)\n \n 2-я неделя: \n 1-я пара: Шабалин (семинар) (2к, 5 этаж)\n 2-я пара: Шабалин (2к, 5 этаж)")
 
 @bot.message_handler(content_types=['text'])
 def echo_all(message):
