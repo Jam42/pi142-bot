6810aedc42c233f3d5536229ba90ea67b89fb8eb
Dmitry Rubtsov <drubtsov@ronasit.com>
2016-11-19 02:04:02 +0600

    switch to yaml

--- bot.py
+++ bot.py
@@ -1,23 +1,20 @@
-# -*- coding: utf-8 -*-
-import telebot
-import json
+
 import os
+import telebot
+import yaml
 
 bot = telebot.TeleBot(os.environ["TELEGRAM_TOKEN"])
 
-with open('schedule.json') as json_data:
-    data = json.load(json_data)
-    schedule = data["Monday"][0]["number"] + ": \n" + data["Monday"][0]["lesson"] + "\n" + data["Monday"][0]["time"] + "\n" + data["Monday"][0]["room"]
+with open('schedule.yaml') as stream:
+    data = yaml.load(stream)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "сообщение деньНедели выдает расписание на этот день")
+	bot.reply_to(message, "hi")
 
-@bot.message_handler(func=lambda message: True)
-def send_message(message):
-	if (message.text == 'понедельник'):
-		bot.send_message(message.chat.id, schedule)
-	#else:
-		#bot.send_message(message.chat.id, 'что')
+@bot.message_handler(commands=['monday','tuesday','wednesday','thursday','friday'])
+def send_day(message):
+	day = yaml.dump(data[message.text[1:]], allow_unicode=True)
+	bot.send_message(message.chat.id, day)
 
 bot.polling()
