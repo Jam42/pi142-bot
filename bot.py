663b8046c43e9d031fd0ce19ec38af757c4b9f5c
jam42 <radiosilence42@gmail.com>
2016-12-04 01:18:38 +0600

    init even_check

--- bot.py
+++ bot.py
@@ -18,24 +18,28 @@ def read_yaml():
     return data
 
 
-@BOT.message_handler(commands=['start'])
-def start_message(message):
+def check_even(message):
     markup = types.ReplyKeyboardMarkup(row_width=2)
     itembtn1 = types.KeyboardButton('True')
     itembtn2 = types.KeyboardButton('False')
     markup.add(itembtn1, itembtn2)
     even = BOT.send_message(message.chat.id, "Is current week even?", reply_markup=markup)
-    return even
-    markup = types.ReplyKeyboardHide(selective=False)
-    BOT.send_message(message.chat.id, message, reply_markup=markup)
+    return BOT.register_next_step_handler(even, even_handler)
 
+
+def even_handler(message):
+    markup = types.ReplyKeyboardHide(selective=False)
+    BOT.send_message(message.chat.id, get_message(read_yaml(), message.text), reply_markup=markup)
+    return message.text
+    
 
 @BOT.message_handler(WEEKDAYS)
 def send_day(message):
     "Send schedule after receive command"
+    BOT.send_message(message.chat.id, check_even(message))
     day_name = message.text[1:]
     day_arr = read_yaml()[day_name]
-    text = ''.join(get_message(day_arr))
+    text = ''.join(get_message(day_arr, check_even(message)))
     BOT.send_message(message.chat.id, text)
 
 
@@ -43,7 +47,8 @@ def send_day(message):
 def send_today(message):
     "Send schedule after receive command"
     day_arr = read_yaml()[get_weekday()]
-    text = ''.join(get_message(day_arr))
+
+    text = ''.join(get_message(day_arr, check_even(message)))
     BOT.send_message(message.chat.id, text)
 
 
@@ -52,17 +57,16 @@ def get_weekday():
     return WEEKDAYS[datetime.datetime.today().weekday() - 1]
 
 
-def get_message(read_yaml):
+def get_message(read_yaml, even):
     "Preparing message to sending"
     data_list = []
     for x in range(len(read_yaml)):
-        if (read_yaml[x]["even"] == "true"): 
+        if read_yaml[x]["even"] == even:
             number = 'Number: ' + read_yaml[x]["number"] + "\n"
             lesson = 'Lesson: ' + read_yaml[x]["lesson"] + "\n"
             time = 'Time: ' + read_yaml[x]["time"] + "\n"
             room = 'Room: ' + read_yaml[x]["room"] + "\n"
-            even = 'Even: ' + read_yaml[x]["even"] + "\n"
-            data_list.append(number + lesson + time + room + even + "\n")
+            data_list.append(number + lesson + time + room + "\n")
     if not data_list:
         return "No lessons"
     return data_list
