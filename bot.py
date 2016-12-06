0d1322e18722dc280fe08194920f5578357f2b43
jam42 <radiosilence42@gmail.com>
2016-12-06 17:19:17 +0600

    adding even_check

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
-
+"Schedule bot"
 import os
 import datetime
 import telebot
 import yaml
-from telebot import types
 
 BOT = telebot.TeleBot(os.environ["TELEGRAM_TOKEN"])
 
@@ -18,28 +16,21 @@ def read_yaml():
     return data
 
 
-def check_even(message):
-    markup = types.ReplyKeyboardMarkup(row_width=2)
-    itembtn1 = types.KeyboardButton('True')
-    itembtn2 = types.KeyboardButton('False')
-    markup.add(itembtn1, itembtn2)
-    even = BOT.send_message(message.chat.id, "Is current week even?", reply_markup=markup)
-    return BOT.register_next_step_handler(even, even_handler)
+def check_even():
+    "Asking for type of week"
+    week_number = datetime.datetime.utcnow().isocalendar()[1]
+    if week_number % 2 != 0:
+        return True
+    else:
+        return False
 
-
-def even_handler(message):
-    markup = types.ReplyKeyboardHide(selective=False)
-    BOT.send_message(message.chat.id, get_message(read_yaml(), message.text), reply_markup=markup)
-    return message.text
-    
 
 @BOT.message_handler(WEEKDAYS)
 def send_day(message):
     "Send schedule after receive command"
-    BOT.send_message(message.chat.id, check_even(message))
     day_name = message.text[1:]
     day_arr = read_yaml()[day_name]
-    text = ''.join(get_message(day_arr, check_even(message)))
+    text = ''.join(get_message(day_arr, check_even()))
     BOT.send_message(message.chat.id, text)
 
 
@@ -47,8 +38,7 @@ def send_day(message):
 def send_today(message):
     "Send schedule after receive command"
     day_arr = read_yaml()[get_weekday()]
-
-    text = ''.join(get_message(day_arr, check_even(message)))
+    text = ''.join(get_message(day_arr, check_even()))
     BOT.send_message(message.chat.id, text)
 
 
@@ -57,15 +47,15 @@ def get_weekday():
     return WEEKDAYS[datetime.datetime.today().weekday() - 1]
 
 
-def get_message(read_yaml, even):
+def get_message(yaml, even):
     "Preparing message to sending"
     data_list = []
-    for x in range(len(read_yaml)):
-        if read_yaml[x]["even"] == even:
-            number = 'Number: ' + read_yaml[x]["number"] + "\n"
-            lesson = 'Lesson: ' + read_yaml[x]["lesson"] + "\n"
-            time = 'Time: ' + read_yaml[x]["time"] + "\n"
-            room = 'Room: ' + read_yaml[x]["room"] + "\n"
+    for x in range(len(yaml)):
+        if yaml[x]['even'] == even:
+            number = 'Number: ' + yaml[x]["number"] + "\n"
+            lesson = 'Lesson: ' + yaml[x]["lesson"] + "\n"
+            time = 'Time: ' + yaml[x]["time"] + "\n"
+            room = 'Room: ' + yaml[x]["room"] + "\n"
             data_list.append(number + lesson + time + room + "\n")
     if not data_list:
         return "No lessons"
